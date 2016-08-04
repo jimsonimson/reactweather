b1784535c3e4d359ed042bc3e369d3058a734574
Mike Hawk <jonathanimson@gmail.com>
2016-08-04 12:39:10 -0700

    added css file

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props)=>{
   return (
     <div>
-      <h3 className="text-center">About</h3>
+      <h3 className="text-center page-title">About</h3>
       <p>This is app is my first react app It is connected to the openweatherapi</p>
       <p>Here are some of the tools I used:</p>
       <ul>
